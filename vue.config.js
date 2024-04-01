613a52000beaaf013e0ef693adcda1061e46db31
Eric Eugene Carter <eric.eugene.carter@gmail.com>
2024-04-01 14:10:09 -0400

    Create polyglot-spa-v2 vue-chess MFE

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+  // options...
+    configureWebpack: {
+        output: {
+            libraryTarget: 'system',
+        },
+    },
+  chainWebpack: config => {
+    // Ignore custom HTML tags/Custom Elements
+    config.module
+        .rule('vue')
+        .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+          options.compilerOptions = {
+            ...(options.compilerOptions || {}),
+            isCustomElement: tag => /^(chess)-/i.test(tag)
+          };
+          return options;
+        });
+  }
+}
